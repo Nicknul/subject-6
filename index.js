21501d5fb2df82b91e2dfc8a173a2847372c728d
정호연 <jea7730@gmail.com>
2024-06-14 17:42:38 +0900

    이벤트리스너 함수 생성 후 버튼을 누르면 AJAX의 방식으로 API가 읽히게 하기

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const fetchBu = document.querySelector('#fetch');
 // post, comment API url 할당
 const post = 'https://jsonplaceholder.typicode.com/posts';
 const comment = 'https://jsonplaceholder.typicode.com/comments?postId=';
-
-ajaxBu.addEventListener('click', () => {});
+// AJAX의 버튼을 누르면 실행
+ajaxBu.addEventListener('click', () => {
+  //AJAX의 방식으로 API 가져오기
+  const postAJAX = new XMLHttpRequestUpload();
+});
