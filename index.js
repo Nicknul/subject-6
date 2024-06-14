78b77cc7fe850867113b303b2e3faa4e1e9907f7
정호연 <jea7730@gmail.com>
2024-06-14 17:40:12 +0900

    post와 comment의 데이터를 담은 url를 변수의 값으로 할당

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// tag 불러오기
 const root = document.getElementById('root');
 const ajaxBu = document.querySelector('#AJAX');
 const fetchBu = document.querySelector('#fetch');
+// post, comment API url 할당
+const post = 'https://jsonplaceholder.typicode.com/posts';
+const comment = 'https://jsonplaceholder.typicode.com/comments?postId=';
+
+ajaxBu.addEventListener('click', () => {});
